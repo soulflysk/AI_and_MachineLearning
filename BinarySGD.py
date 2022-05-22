451b8dedd061d97cdd717287d72e661ec9f80a96
soulflysk <soulflysk@outlook.co.th>
2022-05-22 14:43:39 +0700

    add binargsgd

--- BinarySGD.py
+++ BinarySGD.py
@@ -13,7 +13,13 @@ x,y=mnist["data"],mnist["target"]
 
 x_train, x_test,y_train,y_test= x[:60000],x[60000:],y[:60000],y[60000:]
 
-print(x_train.shape)
-print(x_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+# print(x_train.shape)
+# print(x_test.shape)
+# print(y_train.shape)
+# print(y_test.shape)
+
+predict_number = 5000
+y_test_0 = (y_test==0)
+y_train_0 = (y_train==0)
+print(y_train_0.shape,y_train_0)
+print(y_test_0.shape,y_test_0)
---
+++ EP37.py
@@ -0,0 +1,4 @@
+number=[1,2,3,4,5,6]
+name=["นาย ก","nai 2","nai 3"]
+all=[10,"nai k",True,3.14,-10]
+print(all[-4:-1])
