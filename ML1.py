9c75fa7e5850fb97311722eeb4d8cf816ffb1a79
soulflysk <soulflysk@outlook.co.th>
2022-06-05 19:56:32 +0700

    change ml1

--- ML1.py
+++ ML1.py
@@ -1,7 +1,8 @@
 import pylab
 from sklearn import datasets
 from sklearn.datasets import load_iris
-
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
 digit_dataset = datasets.load_digits()
 
 # print(iris_dataset.keys())
@@ -12,3 +13,20 @@ digit_dataset = datasets.load_digits()
 iris_dataset = load_iris()
 
 print(iris_dataset.data.shape)
+
+mnist_raw=loadmat("mnist-original.mat")
+print(mnist_raw)
+mnist={
+    "data":mnist_raw["data"].T,
+    "target":mnist_raw["label"][0]
+}
+x,y=mnist["data"], mnist["target"]
+number=x[15000]
+number_image=number.reshape(28,28)
+
+plt.imshow(
+        number_image,
+        cmap=plt.cm.binary,
+        interpolation="nearest")
+plt.show()
+print(x.shape)
